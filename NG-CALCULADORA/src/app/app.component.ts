4a0bb6f74422d92fffae5cce4d155c7685ba107b
AndersonFcanel <andersonfcanel@gmail.com>
2019-09-22 10:37:48 -0300

    Aula03

--- NG-CALCULADORA/src/app/app.component.ts
+++ NG-CALCULADORA/src/app/app.component.ts
@@ -1,10 +1,14 @@
 import { Component } from '@angular/core';
 
 @Component({
+/*selector: 'app-root' ==>> Seletor usado lá no index.html. 
+(Verifique que se você trocar o nome do seletor, no index.html também deve ser trocado)
+ */
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'NG-CALCULADORA';
+  //title = 'NG-CALCULADORA' ==>> Título usado no app.component.html por interpolação.
+  title = 'Calculadora';
 }
--- NG-CALCULADORA/src/app/app.module.ts
+++ NG-CALCULADORA/src/app/app.module.ts
@@ -6,12 +6,19 @@ import { AppComponent } from './app.component';
 
 @NgModule({
   declarations: [
+    /*Veja que o AppComponent pertence ao módulo principal do programa.
+      (declarado automaticamente após a criação da aplicação)
+    */
     AppComponent
   ],
   imports: [
+    /*Esses imports são obrigatórios 
+     (recursos de Browser e rota)
+    */
     BrowserModule,
     AppRoutingModule
   ],
+  //Lista de serviços utilizados pelo módulo principal.
   providers: [],
   bootstrap: [AppComponent]
 })
