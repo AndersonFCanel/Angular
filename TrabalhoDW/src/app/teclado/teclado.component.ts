603e1c8e9c9a2b49ba1a9c5b25e758e0bf5cd56b
AndersonFcanel <andersonfcanel@gmail.com>
2019-09-29 11:33:10 -0300

    +

--- TrabalhoDW/src/app/teclado/teclado.component.ts
+++ TrabalhoDW/src/app/teclado/teclado.component.ts
@@ -7,74 +7,117 @@ import { ValidacaoService } from '../validacao.service';
   styleUrls: ['./teclado.component.css']
 })
 export class TecladoComponent implements OnInit {
-  
-  public jogada = [" "," "," "," "," "," "," "," "," "];
+
+  public jogada = [];
   public vez = 0;
   public player1 = "UM";
   public player2 = "DOIS";
+  public pontoPlayer1 = 0;
+  public pontoPlayer2 = 0;
 
-  public marcarReadonly = [false,false,false,false,false,false,false,false,false];
+  public marcarReadonly = [];
 
-
-  
-  constructor(private validacao:ValidacaoService) { }
+  constructor(private validacao: ValidacaoService) { }
 
   ngOnInit() {
+
+    //inicializando os vetores
+    for(var counter:number = 0; counter<9; counter++){
+
+      this.jogada[counter] = "";
+      this.marcarReadonly[counter] = false;
+
+      console.log("for loop executed : " + counter)
+    }
+
   }
 
-readonly(casaTabuleiro:Int)
-{
-  this.marcarReadonly[casaTabuleiro] = true;
-}
+  readonly(casaTabuleiro: number) {
+    this.marcarReadonly[casaTabuleiro] = true;
+  }
 
+  imprimeCasaMarcada() {
+    console.log(this.jogada[0], this.jogada[1], this.jogada[2] + "\n");
+    console.log(this.jogada[3], this.jogada[4], this.jogada[5] + "\n");
+    console.log(this.jogada[6], this.jogada[7], this.jogada[8] + "\n");
 
-imprimeCasaMarcada()
-{
-  console.log( this.jogada[0], this.jogada[1], this.jogada[2]+"\n");
-  console.log( this.jogada[3], this.jogada[4], this.jogada[5]+"\n");
-  console.log( this.jogada[6], this.jogada[7], this.jogada[8]+"\n");
+  }
 
-}
-
-
-
-marcaJogada( casaTabuleiro:Int )
-  {
+  marcaJogada(casaTabuleiro:number) {
     console.log("vez: " + this.vez);
-    
-    if(this.vez%2 == 0)
-    {
-      console.log("Casa marcada " + casaTabuleiro) ;
+
+    if (this.vez % 2 == 0) {
+      console.log("Casa marcada " + casaTabuleiro);
+      
       this.vez = this.vez + 1;
       this.jogada[casaTabuleiro] = "O"
       this.readonly(casaTabuleiro);
+      this.checarVitoria();
 
-      
-    this.imprimeCasaMarcada()
+      this.imprimeCasaMarcada()
 
-      return this.jogada ;
+      return this.jogada;
     }
-    else  
-    {
-      console.log("Casa marcada " + casaTabuleiro) ;
+    else {
+      console.log("Casa marcada " + casaTabuleiro);
+      
       this.vez = this.vez + 1;
       this.jogada[casaTabuleiro] = "X"
       this.readonly(casaTabuleiro);
+      this.checarVitoria();
 
-      
-    this.imprimeCasaMarcada()
+      this.imprimeCasaMarcada()
 
       return this.jogada;
     }
 
   }
 
-  restart(){
-   this.jogada = [" "," "," "," "," "," "," "," "," "];
-   this.vez = 0;
-   this.marcarReadonly = [false,false,false,false,false,false,false,false,false];
- }
+
+  checarVitoria() {
+
+    if( (this.jogada[0] == "X" && this.jogada[1] == "X" && this.jogada[2] == "X") || 
+        (this.jogada[3] == "X" && this.jogada[4] == "X" && this.jogada[5] == "X") ||
+        (this.jogada[6] == "X" && this.jogada[7] == "X" && this.jogada[8] == "X") || 
+        (this.jogada[0] == "X" && this.jogada[3] == "X" && this.jogada[6] == "X") ||
+        (this.jogada[1] == "X" && this.jogada[4] == "X" && this.jogada[7] == "X") ||
+        (this.jogada[2] == "X" && this.jogada[3] == "X" && this.jogada[8] == "X") ||
+        (this.jogada[0] == "X" && this.jogada[4] == "X" && this.jogada[8] == "X") || 
+        (this.jogada[2] == "X" && this.jogada[4] == "X" && this.jogada[6] == "X")) {
+        
+        this.pontoPlayer1 = this.pontoPlayer1 + 1;
+
+        console.log("X venceu");
+        
+        return true;
+
+      } else  
+      if( (this.jogada[0] == "O" && this.jogada[1] == "O" && this.jogada[2] == "O") ||
+          (this.jogada[3] == "O" && this.jogada[4] == "O" && this.jogada[5] == "O") || 
+          (this.jogada[6] == "O" && this.jogada[7] == "O" && this.jogada[8] == "O") ||
+          (this.jogada[0] == "O" && this.jogada[3] == "O" && this.jogada[6] == "O") ||
+          (this.jogada[1] == "O" && this.jogada[4] == "O" && this.jogada[7] == "O") || 
+          (this.jogada[2] == "O" && this.jogada[3] == "O" && this.jogada[8] == "O") || 
+          (this.jogada[0] == "O" && this.jogada[4] == "O" && this.jogada[8] == "O") || 
+          (this.jogada[2] == "O" && this.jogada[4] == "O" && this.jogada[6] == "O") ) {
+
+          this.pontoPlayer2 = this.pontoPlayer2 + 1;
+
+        console.log("O venceu");
+        return true;
+
+      }else
+      {
+        console.log("Ainda não há vencedor");
+        return false;
+      }      
+  }
+
+  restart() {
+   
+    this.ngOnInit() 
+  }
 
 
-  
+
 }
