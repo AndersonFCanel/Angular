17af2af430259b38063deb799a1a19842021ec5d
AndersonFcanel <andersonfcanel@gmail.com>
2019-09-29 03:12:57 -0300

    +

--- TrabalhoDW/src/app/teclado/teclado.component.ts
+++ TrabalhoDW/src/app/teclado/teclado.component.ts
@@ -8,7 +8,7 @@ import { ValidacaoService } from '../validacao.service';
 })
 export class TecladoComponent implements OnInit {
   
-  public jogada = [];
+  public jogada = [" "," "," "," "," "," "," "," "," "];
   public vez = 0;
   public player1 = "UM";
   public player2 = "DOIS";
@@ -27,9 +27,21 @@ readonly(casaTabuleiro:Int)
   this.marcarReadonly[casaTabuleiro] = true;
 }
 
-  marcaJogada( casaTabuleiro:Int )
+
+imprimeCasaMarcada()
+{
+  console.log( this.jogada[0], this.jogada[1], this.jogada[2]+"\n");
+  console.log( this.jogada[3], this.jogada[4], this.jogada[5]+"\n");
+  console.log( this.jogada[6], this.jogada[7], this.jogada[8]+"\n");
+
+}
+
+
+
+marcaJogada( casaTabuleiro:Int )
   {
     console.log("vez: " + this.vez);
+    
     if(this.vez%2 == 0)
     {
       console.log("Casa marcada " + casaTabuleiro) ;
@@ -37,22 +49,31 @@ readonly(casaTabuleiro:Int)
       this.jogada[casaTabuleiro] = "O"
       this.readonly(casaTabuleiro);
 
+      
+    this.imprimeCasaMarcada()
+
       return this.jogada ;
     }
     else  
     {
-      console.log("IMPAR");
+      console.log("Casa marcada " + casaTabuleiro) ;
       this.vez = this.vez + 1;
       this.jogada[casaTabuleiro] = "X"
       this.readonly(casaTabuleiro);
+
       
+    this.imprimeCasaMarcada()
+
       return this.jogada;
     }
 
-
   }
 
-
+  restart(){
+   this.jogada = [" "," "," "," "," "," "," "," "," "];
+   this.vez = 0;
+   this.marcarReadonly = [false,false,false,false,false,false,false,false,false];
+ }
 
 
   
