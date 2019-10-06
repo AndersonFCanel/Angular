0fbee6c7283f86911b29b050331cd33d7016d898
AndersonFcanel <andersonfcanel@gmail.com>
2019-10-05 23:41:34 -0300

    +

--- TrabalhoDW/src/app/informacoes/informacoes.module.ts
+++ TrabalhoDW/src/app/informacoes/informacoes.module.ts
@@ -1,16 +1,17 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { SobreComponent } from './sobre/sobre.component';
-import { ModalInfoComponent } from './modal-info/modal-info.component';
+// { ModalInfoComponent } from './modal-info/modal-info.component';
 
 @NgModule({
-  declarations: [SobreComponent, ModalInfoComponent],
+  declarations: [SobreComponent],//, ModalInfoComponent],
   imports: [
     CommonModule,
     
   ],
   exports: [
     SobreComponent,
+  //  ModalInfoComponent,
   ]
 })
 export class InformacoesModule { }
